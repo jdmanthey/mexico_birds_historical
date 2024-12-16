46275dae2f475ecacc8505e7659b5e5fd00a7ca6
Joe Manthey <jdmanthey@gmail.com>
2024-12-16 14:56:14 -0600

    Create 02_cat_reads.sh

---
+++ 02_cat_reads.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --chdir=./
+#SBATCH --job-name=cat
+#SBATCH --partition nocona
+#SBATCH --nodes=1 --ntasks=1
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-92
+
+source activate hist_filter
+
+# define main working directory
+workdir=/lustre/scratch/jmanthey/08_mexico
+
+basename_array=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/basenames_historical.txt | tail -n1 )
+
+
+cat ${workdir}/00_lane1_data/${basename_array}_R1.fastq.gz >> \
+${workdir}/00_fastq_h/${basename_array}_R1.fastq.gz
+
+cat ${workdir}/00_lane2_data/${basename_array}_R1.fastq.gz >> \
+${workdir}/00_fastq_h/${basename_array}_R1.fastq.gz
+
+cat ${workdir}/00_lane1_data/${basename_array}_R2.fastq.gz >> \
+${workdir}/00_fastq_h/${basename_array}_R2.fastq.gz
+
+cat ${workdir}/00_lane2_data/${basename_array}_R2.fastq.gz >> \
+${workdir}/00_fastq_h/${basename_array}_R2.fastq.gz
+
+
