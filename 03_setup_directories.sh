6903bab199d24aade44e09261e43e994266ba447
Joe Manthey <jdmanthey@gmail.com>
2024-12-30 15:27:22 -0600

    Update 03_setup_directories.sh

--- 03_setup_directories.sh
+++ 03_setup_directories.sh
@@ -1,8 +1,14 @@
+mkdir 00_fastq_h
+mkdir 00_fastq_m
 mkdir 01_cleaned_h
 mkdir 01_cleaned_m
 mkdir 01_split_h
 mkdir 01_working_h
 mkdir 01_bam_files
+mkdir 02_vcf
+mkdir 03_vcf
+mkdir 04_vcf
+mkdir 05_pca
 mkdir 20_combine_lanes
 mkdir 21_split_h_script
 mkdir 22_dedup_h_script
@@ -14,3 +20,5 @@ mkdir 27_align_script_h
 mkdir 28_stats_script_h
 mkdir 29_cat_modern_script
 mkdir 30_align_modern_script
+mkdir 31_genotype_script
+mkdir 32_merge_genotyped_script
