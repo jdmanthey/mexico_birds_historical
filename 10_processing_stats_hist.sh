67aee86b78c507964b636d267b376228a27bf2a5
Joe Manthey <jdmanthey@gmail.com>
2025-05-27 15:47:58 -0500

    Update 10_processing_stats_hist.sh

--- 10_processing_stats_hist.sh
+++ 10_processing_stats_hist.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1 --ntasks=4
 #SBATCH --time=48:00:00
 #SBATCH --mem-per-cpu=8G
-#SBATCH --array=1-81
+#SBATCH --array=1-78
 
 # define main working directory
 workdir=/lustre/scratch/jmanthey/08_mexico
