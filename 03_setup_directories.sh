48d4baa606a6cfeef4aa38a0910ee9f52ebe20a6
Joe Manthey <jdmanthey@gmail.com>
2025-05-29 12:10:45 -0500

    Update 03_setup_directories.sh

--- 03_setup_directories.sh
+++ 03_setup_directories.sh
@@ -9,6 +9,35 @@ mkdir 02_vcf
 mkdir 03_vcf
 mkdir 04_vcf
 mkdir 05_pca
+
+mkdir 06_stats6_atl
+mkdir 06_stats8_atl
+mkdir 06_stats10_atl
+mkdir 06_stats6_atl/windows
+mkdir 06_stats8_atl/windows
+mkdir 06_stats10_atl/windows
+
+mkdir 06_stats6_car
+mkdir 06_stats8_car
+mkdir 06_stats10_car
+mkdir 06_stats6_car/windows
+mkdir 06_stats8_car/windows
+mkdir 06_stats10_car/windows
+
+mkdir 06_stats6_cat
+mkdir 06_stats8_cat
+mkdir 06_stats10_cat
+mkdir 06_stats6_cat/windows
+mkdir 06_stats8_cat/windows
+mkdir 06_stats10_cat/windows
+
+mkdir 06_stats6_myi
+mkdir 06_stats8_myi
+mkdir 06_stats10_myi
+mkdir 06_stats6_myi/windows
+mkdir 06_stats8_myi/windows
+mkdir 06_stats10_myi/windows
+
 mkdir 20_combine_lanes
 mkdir 21_split_h_script
 mkdir 22_dedup_h_script
@@ -22,3 +51,4 @@ mkdir 29_cat_modern_script
 mkdir 30_align_modern_script
 mkdir 31_genotype_script
 mkdir 32_merge_genotyped_script
+mkdir 33_filter_vcf_script
