59040ae004f87d93be99927a14d2584717b7c296
Joe Manthey <jdmanthey@gmail.com>
2026-09-03 14:20:21 -0500

    Update 12_align_modern.sh

--- 12_align_modern.sh
+++ 12_align_modern.sh
@@ -82,7 +82,21 @@ rm ${workdir}/01_bam_files/${basename_array}_cleaned_sorted_rg.bam
 # index the final bam file
 samtools index ${workdir}/01_bam_files/${basename_array}_final.bam
 
+#Mapdamage
+source activate mapdamage
 
+module load  gcc/10.1.0 gsl/2.7 r/4.3.0
+
+# make mapdamage output directory
+mkdir ${workdir}/01_bam_files/temp_${basename_array}
+
+# run mapdamage to rescale bam based on damage estimates
+mapDamage -i ${workdir}/01_bam_files/${basename_array}_final.bam -r ${refgenome} \
+-d ${workdir}/01_bam_files/temp_${basename_array} --merge-reference-sequences --rescale \
+--rescale-out=${workdir}/01c_bam_files/${basename_array}_final.bam
+
+# index the final bam file
+samtools index ${workdir}/01c_bam_files/${basename_array}_final.bam
 
 
 
